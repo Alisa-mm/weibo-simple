d9ca28d60eeadf8d50b2d172a702ad535499873f
lalala <1079662948@qq.com>
2019-11-16 23:04:11 +0800

    点击发布新内容完成

--- 微博案例简单版.js
+++ 微博案例简单版.js
@@ -8,3 +8,31 @@
 2.删除的时候用到事件委托
 3.用到事件对象 获取键盘code值 */
 
+//1. 获取元素
+let btn = document.querySelector('.weibo-btn');
+let text = document.querySelector('.weibo-text');
+let ul = document.querySelector('.weibo-list');
+
+//2.给按钮注册事件
+btn.onclick = function(){
+    // 先得到文本域里面发布的内容
+    content = text.value;
+
+    // 动态生成新的li
+    let li = document.createElement('li');
+    // 给li里面添加内容
+    // li.innerHTML ='<li><p>'+content+'</p><span>删除</span></li>'
+    // 上面的写法可以用模板字符串解决字符串的拼接问题(不用++了)
+    li.innerHTML = ` <li><p>${content}</p><span>删除</span></li>`;
+    // 用document.createElement创建的元素要手动添加进去才有效
+    // 要把后来写的放在前面
+    let first = ul.children[0];
+    ul.insertBefore(li,first);
+
+    // 清空文本域的内容
+    text.value = '';
+    
+    
+    
+
+}
